dafb34c459047ef73daed1e05f1fcedfd6bd7984
Joe <joe_ethan2@hotmail.com>
2025-01-25 14:47:26 +0000

    add: mantine package

--- src/contexts/night-mode.jsx
+++ src/contexts/night-mode.jsx
@@ -1,9 +1,14 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const NightModeContext = createContext();
 
 export const NightModeProvider = ({ children }) => {
   const [isLightMode, setLightMode] = useState(false);
+  const { toggleColorScheme } = useMantineTheme();
+
+  useEffect(() => {
+    toggleColorScheme();
+  }, [isLightMode]);
 
   return (
     <NightModeContext.Provider value={{ isLightMode, setLightMode }}>
--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,19 @@ import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { NightModeProvider } from "./contexts/night-mode.jsx";
 import { IconToggleProvider } from "./contexts/icon-toggle.jsx";
+import "@mantine/core/styles.css";
 import App from "./App.jsx";
 import "./index.css";
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <NightModeProvider>
-      <IconToggleProvider>
-        <App />
-      </IconToggleProvider>
+      <MantineProvider defaultColorScheme="dark">
+        <IconToggleProvider>
+          <App />
+        </IconToggleProvider>
+      </MantineProvider>
     </NightModeProvider>
   </BrowserRouter>
 );
